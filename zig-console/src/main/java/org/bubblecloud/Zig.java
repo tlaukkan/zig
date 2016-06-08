f5c6ed34d2cccfe43f378a4f46309311e328b496
tlaukkan <tommi.laukkanen@noona.com>
2016-06-08 17:54:19 +0300

    Updated Zig according to latest changes in zigbee4java master.

--- zig-console/src/main/java/org/bubblecloud/Zig.java
+++ zig-console/src/main/java/org/bubblecloud/Zig.java
@@ -1,13 +1,12 @@
 package org.bubblecloud;
 
-import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+
 import org.apache.log4j.xml.DOMConfigurator;
-import org.bubblecloud.zigbee.ZigBeeConsoleApiClient;
-import org.bubblecloud.zigbee.ZigBeeConsoleRpcApi;
+import org.bubblecloud.zigbee.ZigBeeConsoleClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +19,7 @@ public class Zig {
     /**
      * The logger.
      */
-    private final static Logger LOGGER = LoggerFactory.getLogger(Zig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Zig.class);
     /**
      * Flag reflecting whether shutdown is requested.
      */
@@ -34,8 +33,8 @@ public class Zig {
     public static void main(final String[] args) throws IOException {
         DOMConfigurator.configure("log4j.xml");
 
-        final ZigBeeConsoleApiClient zigBeeConsoleApiClient = new ZigBeeConsoleApiClient(args[1], args[2]);
-        final ZigBeeConsoleRpcApi zigBeeConsoleApi = zigBeeConsoleApiClient.getZigBeeConsoleApi();
+        final ZigBeeConsoleClient zigbeeConsoleClient = new ZigBeeConsoleClient(args[1], args[2]);
+        zigbeeConsoleClient.startup();
 
         final SlackSession session = SlackSessionFactory.createWebSocketSlackSession(args[0]);
         session.connect();
@@ -47,7 +46,7 @@ public class Zig {
                 public void onEvent(final SlackMessagePosted event, final SlackSession session) {
                     if (!zigId.equals(event.getSender().getId())
                             && event.getChannel().isDirect()) {
-                        session.sendMessage(event.getChannel(), zigBeeConsoleApi.execute(event.getMessageContent()));
+                        session.sendMessage(event.getChannel(), zigbeeConsoleClient.execute(event.getMessageContent()));
                     }
                 }
             });
@@ -68,6 +67,7 @@ public class Zig {
             LOGGER.error("Error in Zig, exiting. ", e);
         } finally {
             session.disconnect();
+            zigbeeConsoleClient.shutdown();
         }
     }
 
